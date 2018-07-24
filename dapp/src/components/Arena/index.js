8bb9a390f31393b52b8bafc1b35419a935cbefcd
Jacky <jacky@Jackyde-MacBook-Pro.local>
2018-07-24 12:11:07 +0800

    styling Arena's head

--- dapp/src/components/Arena/index.js
+++ dapp/src/components/Arena/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import classnames from 'classnames/bind';
 import style from './Arena.css';
-import {TweenMax} from "gsap/TweenMax";
+import { TweenMax, } from "gsap/TweenMax";
+import cardTitleImg from '../../images/cardtitle.png';
 const cx = classnames.bind(style);
 
 export default class extends React.Component {
@@ -10,12 +11,20 @@ export default class extends React.Component {
     
     return (
       <div className={cx('arena', { open: isShowArena })}>
-        ALKSDJFLKSDJFSLKDFJSLDFSDL
-        IRUWEOIRUEORWIRUOWEIRUWEORIWUOIUEWORE
-        XMCNVXCZCZXCXZC
+        <div className="cloud_card1"></div>
+        <div className="cloud_card2"></div>
+        <div className="ui start1"></div>
+        <div className="ui start2"></div>
+        <div className="ui start3"></div>
+        <div className="ui Elf1"></div>
+        <div className="ui Elf2"></div>
+        <div className="ui Elf3"></div>
 
+        <div className="card-title">
+          <img src={ cardTitleImg } />
+          <a className="go-back" onClick={handleBack}></a>
+        </div>
 
-        <a onClick={handleBack}>BACK</a>
       </div>
     )
 
