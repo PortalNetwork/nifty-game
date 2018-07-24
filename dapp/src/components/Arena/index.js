cee185d0f55aa33450ae295f3d93732a2e293a28
Jacky <jacky@Jackyde-MacBook-Pro.local>
2018-07-24 17:02:59 +0800

    add basic validation for bet

--- dapp/src/components/Arena/index.js
+++ dapp/src/components/Arena/index.js
@@ -9,39 +9,80 @@ import historyImg from '../../images/history.png';
 const cx = classnames.bind(style);
 
 export default class extends React.Component {
+  state = {
+    cards: [
+      '1213321',
+      '1213322',
+      '1213323',
+      '1213324',
+    ],
+    selectedCard: '1213322',
+    betEth: 0.01,
+    isShowResult: false,
+    isShowHistory: false,
+  }
+
+  handleBetETHChange = e => {
+    let betEth = e.target.value;
+    if(betEth > 1) {
+      alert('bet eth should not bigger than 1');
+      betEth = 0.01;
+    }
+
+    this.setState({
+      betEth,
+    });
+  }
+
+  handleSelectChange = e => {
+    this.setState({
+      selectedCard: e.target.value,
+    });
+  }
+
+  handlePlaceBet = e => {
+    const { betEth, selectedCard, } = this.state;
+    if(betEth > 1 || betEth < 0.01) {
+      alert('bet eth should not be bigger than 1 and less than 0.01');
+      return;
+    }
+    alert('pass');
+
+  }
+
   render() {
+    const { cards, selectedCard, betEth, isShowResult, isShowHistory, } = this.state;
     const { isShowArena, handleBack, } = this.props;
     
     return (
       <div className={cx('arena', { open: isShowArena })}>
         <div className="cloud_card1"></div>
         <div className="cloud_card2"></div>
-        { false && <div className="ui bg_footer"></div>}
+        <div className="ui bg_footer"></div>
 
         <div className="card-title">
           <img src={ gameplaytitleImg } />
           <a className="go-back" onClick={handleBack}></a>
         </div>
 
-
+        { /* 開局 */}
         <div className={cx('battle-field')}>
           <div className={cx('left')}>
             <div className={cx('left-item')}>
               <label className={cx('select_card_field')} for="select-card">
-                <select id="select-card">
-                  <option>1213321</option>
-                  <option>1213322</option>
-                  <option>1213323</option>
-                  <option>1213324</option>
+                <select id="select-card" value={selectedCard} onChange={this.handleSelectChange}>
+                {
+                  cards.map(card => (<option value={card}>{card}</option>))
+                }
                 </select>
               </label>
             </div>
 
             <div className={cx('left-item')}>
               <span className={cx('bet_eth_field')}>
-                <input type="number" />
+                <input type="number" name="betEth" value={betEth} onChange={this.handleBetETHChange} />
               </span>
-              <a onClick={()=>{alert('bet')}}>
+              <a onClick={this.handlePlaceBet}>
                 <img className={cx('place_bet_button')} src={playgameImg} />
               </a>
             </div>
@@ -59,10 +100,21 @@ export default class extends React.Component {
           </div>
         </div>
 
+        { /* 戰鬥結果 */ }
+        {
+          isShowResult && 
+          <div className={cx('battle-result')}>
 
+          </div>
+        }
 
+        { /* 歷史訊息 */}
+        {
+          isShowHistory && 
+          <div className={cx('battle-history')}>
 
-        
+          </div>
+        }
       </div>
     )
 
